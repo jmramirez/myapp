13feb634c6399f6b9c444b78b8ffb38b46b2050b
Jose M Ramirez Ramirez <josemramirezramirez@Joses-MBP.lan>
2019-02-04 12:52:20 -0500

    Configuring Sass

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-alert('Hello Word')
+import './sass/main.scss';
+
+document.body.innerHTML = "<h1>Hello we appended this!!</h1>"
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,59 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
     entry: {
-      app: './scr/index.js'
+      app: './src/index.js'
+    },
+    optimization:{
+      splitChunks: {
+          cacheGroups: {
+              styles: {
+                  name: 'styles',
+                  test: /\.css$/,
+                  chunks: 'all',
+                  enforce: true
+              }
+          }
+      }
     },
     devServer: {
         hot: true,
-        compress: true
+        compress: true,
+        contentBase: path.join(__dirname, 'dist'),
+        open: 'Google Chrome'
     },
     watch: true,
     devtool: 'source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[name].css"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
     }
 }
